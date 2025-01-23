30b705b3c02c93c2475a725d774b307e6cd12e7b
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2025-01-23 10:00:32 +0700

    feat(tcp): set up the server

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                tokio::spawn(handle_connection(stream));
+            }
+            Err(e) => {
+                println!("error: {}", e);
+                continue;
+            }
+        }
+    }
+}
+
+async fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    loop {
+        match stream.read(&mut buffer).await {
+            Ok(size) if size != 0 => {
+                let response = "+PONG\r\n";
+
+                if let Err(e) = stream.write_all(response.as_bytes()).await {
+                    eprintln!("Error writing to socket: {}", e);
+                }
+            }
+            Ok(_) => {
+                println!("Connection closed");
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+        }
+    }
+}
